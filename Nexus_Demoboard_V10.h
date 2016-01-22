93f029bde63a9ad3ecc825c99b086f5982510386
Gerben <gerben@ideetron.nl>
2016-01-22 11:57:53 +0100

    Create Nexus_Demoboard_V10.h

---
+++ Nexus_Demoboard_V10.h
@@ -0,0 +1,45 @@
+/******************************************************************************************
+*
+* File:        Nexus_Demoboard_V10.h
+* Author:      Gerben den Hartog
+* Compagny:    Ideetron B.V.
+* Website:     http://www.ideetron.nl/LoRa
+* E-mail:      info@ideetron.nl
+******************************************************************************************/
+
+/****************************************************************************************
+*
+* Created on: 			15-01-2016
+* Supported Hardware:	ID150170-01 with ID150119-02 with a RFM95
+*
+* History:
+*
+* Firmware version: 1.0
+* First version
+****************************************************************************************/
+
+#ifndef NEXUS_DEMOBOARD_V10_H
+#define NEXUS_DEMOBOARD_V10_H
+
+/*
+*****************************************************************************************
+* DEFINES
+*****************************************************************************************
+*/
+
+#define MOV     A0
+#define LED1    A1
+#define LED2    A2
+#define PIR     A3
+#define RES     A6
+#define LDR     A7
+#define DS2401  2
+#define MFP     3
+#define DIO0    4
+#define DIO1    5
+#define DIO5    6
+#define DIO2    7
+#define CS      8
+#define LED_N   9
+
+#endif
