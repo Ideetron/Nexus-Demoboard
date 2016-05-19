188ba31d51d8e2be93239e73692f8d52bd0418d3
Gerben <gerben@ideetron.nl>
2016-05-19 11:33:33 +0200

    Create Nexus_Demoboard_V20.h

---
+++ V20/Nexus_Demoboard_V20.h
@@ -0,0 +1,48 @@
+/******************************************************************************************
+*
+* File:        Nexus_Demoboard_V20.h
+* Author:      Gerben den Hartog
+* Compagny:    Ideetron B.V.
+* Website:     http://www.ideetron.nl/LoRa
+* E-mail:      info@ideetron.nl
+******************************************************************************************/
+
+/****************************************************************************************
+*
+* Created on: 			19-05-2016
+* Supported Hardware:	ID150170-01 with ID150119-02 with a RFM95
+*
+* History:
+*
+* Firmware version: 1.0
+* First version
+* Firmware version: 2.0
+* New using Encrypt version 3.1
+* Using AppsKey for encryption of data insted of NwkSkey
+****************************************************************************************/
+
+#ifndef NEXUS_DEMOBOARD_V20_H
+#define NEXUS_DEMOBOARD_V20_H
+
+/*
+*****************************************************************************************
+* DEFINES
+*****************************************************************************************
+*/
+
+#define MOV     A0
+#define LED1    A1
+#define LED2    A2
+#define PIR     A3
+#define RES     A6
+#define LDR     A7
+#define DS2401  2
+#define MFP     3
+#define DIO0    4
+#define DIO1    5
+#define DIO5    6
+#define DIO2    7
+#define CS      8
+#define LED_N   9
+
+#endif
